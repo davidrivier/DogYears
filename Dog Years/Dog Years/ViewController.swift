e416690806d5131aa048b7a7475ea98689bf995d
David Rivier <david@rivier.me>
2014-09-14 22:28:01 +0100

    Dog years excercise - 1

---
+++ Dog Years/Dog Years/ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  Dog Years
+//
+//  Created by David on 14/09/2014.
+//  Copyright (c) 2014 Southstep. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    
+    
+    @IBOutlet weak var humanYearsTexfield: UITextField!
+    
+    
+    @IBOutlet weak var dogYearsLabel: UILabel!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+    @IBAction func convertHumanIntoYears(sender: UIButton) {
+        
+        
+        let dogYears = humanYearsTexfield.text.toInt()!
+        
+        let constantHumanToDog = 7
+        
+        dogYearsLabel.hidden = false
+        
+        humanYearsTexfield.resignFirstResponder()
+        
+        dogYearsLabel.text = "\(dogYears * constantHumanToDog)" + " in dog years"
+        
+        
+        
+        
+    }
+    
+    
+
+
+}
+
